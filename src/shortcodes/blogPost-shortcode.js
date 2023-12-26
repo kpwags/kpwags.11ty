263008a659ebd8d9ca9c9e3d49a061c41edc28e9
Keith Wagner <244694+kpwags@users.noreply.github.com>
2023-12-26 14:58:51 -0500

    Condensed Blog Posts & Book Notes into Shortcodes
    # Work Done
    Moved blog posts, reading logs, & book notes to use shortcodes rather than duplicate the templates.
    
    # Associated Issue
    - Closes #56

---
+++ src/shortcodes/blogPost-shortcode.js
@@ -0,0 +1,20 @@
+const toHTML = require('../filters/tohtml-filter');
+const readableDate = require('../filters/date-filter');
+const readingTime = require('../filters/readingTime-filter');
+
+module.exports = (post) => {
+	const readingTimeDiv = !post.data.title.includes('Reading Log - ')
+		? `<div>&bull;</div> <div>${readingTime(post)}</div>`
+		: '';
+
+	return `
+<article class="post">
+	<h2><a href="${post.url}">${post.data.title}</a></h2>
+	<div class="metadata">
+		<div class="post-date">${readableDate(post.data.date)}</div>
+		${readingTimeDiv}
+	</div>
+	<div class="excerpt">${toHTML(post.data.page.excerpt)}</div>
+</article>
+`;
+}
---
+++ src/shortcodes/bookNote-shortcode.js
@@ -0,0 +1,32 @@
+const starRating = require('./starRating-shortcode');
+const readableDate = require('../filters/date-filter');
+
+module.exports = (bookNote) => {
+	const categories = [];
+
+	bookNote.data.categories.forEach((category) => {
+		categories.push(`<span class="category">${category}</span>`);
+	});
+
+	return `
+<article class="book-note-card">
+	<div class="content">
+		<div class="image">
+			<img src="${bookNote.data.coverImage}" alt="${bookNote.data.author} - ${bookNote.data.title}" />
+		</div>
+		<div>
+			<h2><a href="${bookNote.url}">${bookNote.data.title}</a></h2>
+			<h3>by ${bookNote.data.author}</h3>
+
+			<div class="metadata">${readableDate(bookNote.date)}</div>
+
+			${starRating(bookNote.data.rating)}
+
+			<div class="categories">
+				${categories.join('')}
+			</div>
+		</div>
+	</div>
+</article>
+	`;
+};
