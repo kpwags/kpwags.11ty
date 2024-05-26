a95b2a8a4b55acf79f6744ea7cad9bc73c876ff7
Keith Wagner <github@kpwags.com>
2024-05-26 18:58:36 -0400

    Added Pixelfed to Homepage & Updated Homepage

---
+++ src/_data/pixelfed.js
@@ -0,0 +1,36 @@
+const { XMLParser } = require('fast-xml-parser');
+
+module.exports = () => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const images = [];
+
+			const res = await fetch('https://pixelfed.social/users/kpwags.atom');
+
+			if (!res.ok) {
+				reject('error during fetch');
+			}
+
+			const response = await res.text();
+
+			const options = {
+			    ignoreAttributes : false
+			};
+
+			const parser = new XMLParser(options);
+			const data = parser.parse(response);
+
+			data.feed.entry.forEach((i) => {
+				images.push({
+					link: i.id,
+					title: i.summary['#text'],
+					image: i['media:content']['@_url'],
+				});
+			});
+
+			resolve(images.slice(0, 3));
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
