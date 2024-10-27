01ddc43cbd629a09e9f9dbba12dc30fd5e979c53
Keith Wagner <github@kpwags.com>
2024-10-27 18:40:41 -0400

    Reversed Date & Author for Notes

--- src/shortcodes/noteListing-shortcode.js
+++ src/shortcodes/noteListing-shortcode.js
@@ -2,9 +2,9 @@ const noteListingShortcode = (note) => `
 <article class="note">
 	<h2><a href="${note.url}">${note.data.title}</a></h2>
 	<div class="metadata">
-		<div class="post-date">${note.data.author}</div>
-		<div class="separator">&bull;</div>
 		<div class="post-date">${note.data.dateString}</div>
+		<div class="separator">&bull;</div>
+		<div class="post-date">${note.data.author}</div>
 	</div>
 	<div class="content">${note.templateContent}</div>
 	<div class="view-link"><a href="${note.data.link}">Read More</a></div>
