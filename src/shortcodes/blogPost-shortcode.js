914a017f63100064481770193525cae2f4fea607
Keith Wagner <github@kpwags.com>
2025-03-20 19:23:19 -0400

    Started Work on Redesign

--- src/shortcodes/blogPost-shortcode.js
+++ src/shortcodes/blogPost-shortcode.js
@@ -2,7 +2,7 @@ import toHTML from '../filters/tohtml-filter.js';
 
 const blogPostShortcode = (post, type) => {
 	return `
-<article class="post ${type}">
+<article class="post-snippet ${type}">
 	<div class="content">
 		<h2><a href="${post.url}">${post.data.title}</a></h2>
 		<div class="metadata">${post.data.dateString}</div>
