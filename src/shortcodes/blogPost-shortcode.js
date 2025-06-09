69b5d57d83df1e7e2f120b7a4ab28ec957d392b4
Keith Wagner <github@kpwags.com>
2025-06-09 18:23:41 -0400

    New Reading Log - 126

--- src/shortcodes/blogPost-shortcode.js
+++ src/shortcodes/blogPost-shortcode.js
@@ -2,14 +2,14 @@ import toHTML from '../filters/tohtml-filter.js';
 
 const blogPostShortcode = (post, type) => {
 	return `
-<article class="post-snippet ${type}">
+<article class="post-snippet ${type === 'weeknote' ? 'blogpost' : type}">
 	<div class="content">
 		<h2><a href="${post.url}">${post.data.title}</a></h2>
 		<div class="metadata">${post.data.dateString}</div>
-		<div class="excerpt">${toHTML(post.data.page.excerpt)}</div>
+		${type === 'weeknote' ? '' : `<div class="excerpt">${toHTML(post.data.page.excerpt)}</div>`}
 	</div>
 </article>
-<hr />
+${type === 'weeknote' ? '' : '<hr />'}
 `;
 };
 
