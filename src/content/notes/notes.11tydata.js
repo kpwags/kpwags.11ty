cb3fd60bf911cb7debaba67be7880205b94be593
Keith Wagner <github@kpwags.com>
2026-07-25 19:46:50 -0400

    Redesigned Homepage

--- src/content/notes/notes.11tydata.js
+++ src/content/notes/notes.11tydata.js
@@ -9,6 +9,7 @@ export default {
 	eleventyComputed: {
 		dateString: ({ page }) => dayjs.utc(page.date).format('MMMM D, YYYY'),
 		shortDateString: ({ page }) => dayjs.utc(page.date).format('MMM D'),
+		dateStringDMYYYY: ({ page }) => dayjs.utc(page.date).format('D MMM YYYY'),
 		postMonth: ({ page }) => dayjs.utc(page.date).format('MM'),
 		postYear: ({ page }) => dayjs.utc(page.date).format('YYYY'),
 		postType: { name: 'Note', css: 'note' },
--- src/content/posts/posts.11tydata.js
+++ src/content/posts/posts.11tydata.js
@@ -4,14 +4,15 @@ import utc from 'dayjs/plugin/utc.js';
 dayjs.extend(utc);
 
 export default {
-    layout: 'layouts/blog-post.html',
-    tags: 'post',
-    eleventyComputed: {
-        dateString: ({ page }) => dayjs.utc(page.date).format('MMMM D, YYYY'),
-        archiveDateString: ({ page }) => dayjs.utc(page.date).format('M-D'),
-        shortDateString: ({ page }) => dayjs.utc(page.date).format('MMM D'),
-        postMonth: ({ page }) => dayjs.utc(page.date).format('MM'),
-        postYear: ({ page }) => dayjs.utc(page.date).format('YYYY'),
+	layout: 'layouts/blog-post.html',
+	tags: 'post',
+	eleventyComputed: {
+		dateString: ({ page }) => dayjs.utc(page.date).format('MMMM D, YYYY'),
+		archiveDateString: ({ page }) => dayjs.utc(page.date).format('M-D'),
+		dateStringDMYYYY: ({ page }) => dayjs.utc(page.date).format('D MMM YYYY'),
+		shortDateString: ({ page }) => dayjs.utc(page.date).format('MMM D'),
+		postMonth: ({ page }) => dayjs.utc(page.date).format('MM'),
+		postYear: ({ page }) => dayjs.utc(page.date).format('YYYY'),
 		postType: { name: 'Post', css: 'post' },
-    }
+	}
 };
