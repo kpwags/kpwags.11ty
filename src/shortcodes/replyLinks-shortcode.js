107253dda4ab3eb8551fa60cd95e160642654f83
Keith Wagner <github@kpwags.com>
2026-07-31 09:50:52 -0400

    Added New Blog Note Page

--- src/shortcodes/replyLinks-shortcode.js
+++ src/shortcodes/replyLinks-shortcode.js
@@ -1,11 +1,13 @@
 const replyLinksShortcode = (title) => {
 	return `
-		<ul class="titled-horizontal-list">
-			<li><strong>Reply via:</strong></li>
-			<li><a href="mailto:hello@kpwags.com?subject=${title}">Email</a></li>
-			<li><a href="https://hachyderm.io/@kpwags" target="_blank" rel="noreferrer nofollow">Mastodon</a></li>
-			<li><a href="https://bsky.app/profile/kpwags.com" target="_blank" rel="noreferrer nofollow">Bluesky</a></li>
-		</ul>
+		<div class="reply-via">
+			<span>Reply via:</span>
+			<ul>
+				<li><a href="mailto:hello@kpwags.com?subject=${title}">Email</a></li>
+				<li><a href="https://hachyderm.io/@kpwags" target="_blank" rel="noreferrer nofollow">Mastodon</a></li>
+				<li><a href="https://bsky.app/profile/kpwags.com" target="_blank" rel="noreferrer nofollow">Bluesky</a></li>
+			</ul>
+		</div>
 	`;
 };
 
--- src/shortcodes/tagList-shortcode.js
+++ src/shortcodes/tagList-shortcode.js
@@ -4,11 +4,20 @@ const tagListShortcode = (tags) => {
 	const renderedTags = tags
 		.filter((t) => !['post', 'readinglog', 'shortnotes', 'weeknote'].includes(t));
 
-	const links = renderedTags.map((tag) => `<li><a href="/tag/${tagUrlFilter(tag)}">${tag}</a></li>`);
+	const links = renderedTags.map((tag) => `
+		<li>
+			<a href="/tag/${tagUrlFilter(tag)}">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" aria-hidden="true">
+					<path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+					<path d="M1.293 7.793A1 1 0 0 1 1 7.086V2a1 1 0 0 0-1 1v4.586a1 1 0 0 0 .293.707l7 7a1 1 0 0 0 1.414 0l.043-.043z"/>
+				</svg>
+				<span>${tag}</span>
+			</a>
+		</li>
+	`);
 
 	return `
-		<ul class="titled-horizontal-list">
-			<li><strong>Tagged:</strong></li>
+		<ul class="tag-list">
 			${links.join('')}
 		</ul>
 	`;
