801028c35a81ef7d92287cf866fb3315dad5db08
Keith Wagner <github@kpwags.com>
2026-09-12 17:34:28 -0400

    New Post - 1Password

--- tools/populator/config.js
+++ tools/populator/config.js
@@ -1,7 +1,7 @@
 export const config = {
 	build: '1.0.1',
-	apiUrl: 'http://192.168.1.232:3010',
-	// apiUrl: 'http://127.0.0.1:3010',
+	apiUrl: 'http://192.168.1.232:3030/media',
+	// apiUrl: 'http://127.0.0.1:3030/media',
 	outputDirectory: {
 		linux: '/home/keith/Developer/kpwags.com/src/_data',
 		mac: '/Users/keith/Developer/kpwags.com/src/_data',
